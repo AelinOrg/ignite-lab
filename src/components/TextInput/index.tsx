6aa56f781d7c071bb2a540ed14d5b42c109499f9
marco <rezende@espressolabs.com.br>
2022-10-25 22:36:21 -0300

    update

--- src/components/TextInput/index.tsx
+++ src/components/TextInput/index.tsx
@@ -1,13 +1,50 @@
 import { Slot } from "@radix-ui/react-slot";
-import { InputHTMLAttributes } from "react";
+import { InputHTMLAttributes, ReactNode } from "react";
 
-export interface TextInputProps extends InputHTMLAttributes<HTMLInputElement> {}
+// Desenvolvimento seguindo o conceito de composição.
 
-export function TextInput(props: TextInputProps) {
+/* ------- Root ------- */
+export interface TextInputRootProps {
+  children: ReactNode;
+}
+
+function TextInputRoot({ children }: TextInputRootProps) {
+  return (
+    <div className="flex items-center h-12 gap-3 py-4 px-3 bg-gray-800 rounded w-full  transition-colors focus:ring-2 ring-cyan-600">
+      {children}
+    </div>
+  );
+}
+
+TextInputRoot.displayName = "TextInput.Root";
+
+/* ------- Icon ------- */
+export interface TextInputIconProps {
+  children: ReactNode;
+}
+
+function TextInputIcon({ children }: TextInputIconProps) {
+  return <Slot className="w-6 h-6 text-gray-400">{children}</Slot>;
+}
+
+TextInputIcon.displayName = "TextInput.Icon";
+
+/* ------- Input ------- */
+export type TextInputInputProps = InputHTMLAttributes<HTMLInputElement>;
+
+function TextInputInput(props: TextInputInputProps) {
   return (
     <input
-      className="py-4 px-3 bg-gray-800 rounded w-full outline-none text-gray-100 text-sm placeholder:text-gray-400 transition-colors focus:ring-2 ring-cyan-600"
+      className="bg-transparent flex-1 outline-none text-gray-100 text-sm placeholder:text-gray-400"
       {...props}
     />
   );
 }
+
+TextInputInput.displayName = "TextInput.Input";
+
+export const TextInput = {
+  Root: TextInputRoot,
+  Input: TextInputInput,
+  Icon: TextInputIcon,
+};
